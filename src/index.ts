ba7d5a68078f3edbca13d8257561ab414625442e
labarruffe <lucas_barruffe@hotmail.com>
2019-07-16 16:55:38 -0300

    Add index class

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import {connect} from 'mongoose';
+import {app} from './app';
+
+async function initialize() {
+    const url = 'mongodb://localhost:27017/fastfood_store';
+    await connect(url, {useNewUrlParser: true});
+    console.log('Conectado com sucesso em fastfood_store!');
+    app.listen(3000, () => console.log('Executando na porta 3000.'));
+
+} 
+
+initialize();
